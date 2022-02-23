de78c6f3c6e01b932db39670cc9af67802afa657
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-02-23 22:20:04 +0530

    home

--- app/src/main/java/com/example/map_my_sona/Home.java
+++ app/src/main/java/com/example/map_my_sona/Home.java
@@ -2,7 +2,13 @@ package com.example.map_my_sona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        MaterialButton a=findViewById(R.id.admin);
+        MaterialButton b=findViewById(R.id.user);
+
+
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(Home.this,login.class);
+                startActivity(a);
+
+//                Toast.makeText(Home.this,"Successfully logged in",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b=new Intent(Home.this,login.class);
+                startActivity(b);
+
+//                Toast.makeText(Home.this,"Successfully logged in",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

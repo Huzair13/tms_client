4144e1a84e06ad77668007d34c3acf8f05ce04ee
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-02-23 20:45:46 +0530

    Splash added

--- app/src/main/java/com/example/map_my_sona/MainActivity.java
+++ app/src/main/java/com/example/map_my_sona/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.map_my_sona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int sleeptimer=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+         @Override
+           public void run() {
+              Intent i=new Intent(MainActivity.this,Home.class);
+              startActivity(i);
+              }
+             },sleeptimer
+        );
     }
-}
+    }

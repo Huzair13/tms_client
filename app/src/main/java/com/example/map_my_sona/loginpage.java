726101dac77d10f7efdf7ae5b84bf42b80ec564f
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-02-24 21:49:41 +0530

    qrcodepage added

--- app/src/main/java/com/example/map_my_sona/loginpage.java
+++ app/src/main/java/com/example/map_my_sona/loginpage.java
@@ -12,20 +12,12 @@ public class loginpage extends AppCompatActivity {
 
 
 
-    MaterialButton login = findViewById(R.id.loginbutton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a=new Intent(loginpage.this , dashboard.class);
-                startActivity(a);
-            }
-        });
 
 
     }

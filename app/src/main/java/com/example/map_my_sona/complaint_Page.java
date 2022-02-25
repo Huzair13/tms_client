e2461b4a8c010c55e269bb67a54358ac5c43b44c
Huzair13 <ahamedhuzair13@gmail.com>
2022-02-25 12:09:15 +0530

    changes

--- app/src/main/java/com/example/map_my_sona/complaint_Page.java
+++ app/src/main/java/com/example/map_my_sona/complaint_Page.java
@@ -3,12 +3,16 @@ package com.example.map_my_sona;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class complaint_Page extends AppCompatActivity {
+    public static TextView scanText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint_page);
+
+        scanText=(TextView) findViewById(R.id.textView);
     }
 }
--- app/src/main/java/com/example/map_my_sona/dashboard.java
+++ app/src/main/java/com/example/map_my_sona/dashboard.java
@@ -2,13 +2,27 @@ package com.example.map_my_sona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class dashboard extends AppCompatActivity {
+
+    private MaterialCardView scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        scanner=findViewById(R.id.card1);
+        scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(dashboard.this,ScannerPage.class));
+            }
+        });
     }
 }

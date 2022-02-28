dc1a990d430d5d3e1f45d2833621e5bf4cb57d37
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-02-28 13:34:00 +0530

    messaage

---
+++ app/src/main/java/com/example/map_my_sona/historyviewdetails.java
@@ -0,0 +1,14 @@
+package com.example.map_my_sona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historyviewdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historyviewdetails);
+    }
+}

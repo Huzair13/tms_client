48c1edad743d3fa9283973fd48823599bda2f57b
Huzair13 <ahamedhuzair13@gmail.com>
2022-03-01 12:15:54 +0530

    admin dashboard drawer layout

--- app/src/main/java/com/example/map_my_sona/admin_dashboard.java
+++ app/src/main/java/com/example/map_my_sona/admin_dashboard.java
@@ -1,16 +1,96 @@
 package com.example.map_my_sona;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class admin_dashboard extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class admin_dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    MaterialToolbar toolbar;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
 
+        drawerLayout=findViewById(R.id.admin_Drawer_Layout);
+        navigationView=findViewById(R.id.nav_view_admin);
+        toolbar=findViewById(R.id.topAppBar_admin);
 
+        builder=new AlertDialog.Builder(this);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(admin_dashboard.this);
+
+        navigationView.setCheckedItem(R.id.nav_home_admin);
+
+        View headerview=navigationView.getHeaderView(0);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.nav_home_admin:
+                startActivity(new Intent(admin_dashboard.this, admin_dashboard.class));
+                break;
+
+            case R.id.nav_logOut:
+                builder.setTitle("Alert")
+                        .setMessage("Are you sure to Log out")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent=new Intent(admin_dashboard.this, AdminLoginPage.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .show();
+                break;
+        }
+
+        return true;
     }
 }

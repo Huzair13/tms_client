a9043da849e5672f9bc3eb128057676838ca8bb4
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-04-03 10:41:03 +0530

    added : manual entry view

---
+++ app/src/main/java/com/example/map_my_sona/Manual_entry_view.java
@@ -0,0 +1,14 @@
+package com.example.map_my_sona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manual_entry_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_entry_view);
+    }
+}

f577f2e0c0992616049e00862494724ea2bc0711
sriee19 <srisanjanaarunkumar@gmail.com>
2022-04-11 16:04:28 +0530

    email

--- app/src/main/java/com/example/map_my_sona/complaints/complaint_Page.java
+++ app/src/main/java/com/example/map_my_sona/complaints/complaint_Page.java
@@ -212,7 +212,6 @@ public class complaint_Page extends AppCompatActivity {
 //                            "Testing",
 //                            "Its working good");
 //                    mail.execute();
-
                     submitComplaint();
                 }
 

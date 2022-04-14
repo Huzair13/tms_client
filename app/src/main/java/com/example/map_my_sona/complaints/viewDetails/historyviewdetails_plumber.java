ba0c74cad31d29527ffd7e731152e2dda17c7c81
sriee19 <srisanjanaarunkumar@gmail.com>
2022-04-14 17:28:44 +0530

    emergency

--- app/src/main/java/com/example/map_my_sona/complaints/viewDetails/historyviewdetails_plumber.java
+++ app/src/main/java/com/example/map_my_sona/complaints/viewDetails/historyviewdetails_plumber.java
@@ -242,7 +242,7 @@ public class historyviewdetails_plumber extends AppCompatActivity {
 
                                 }
                                 else{
-                                    Toast.makeText(historyviewdetails_plumber.this, "Complaint is not made by you so cant close it", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(historyviewdetails_plumber.this, "You are unable to close this complaint , as it is not filled by you.", Toast.LENGTH_SHORT).show();
                                 }
                             }
                         }
@@ -301,9 +301,9 @@ public class historyviewdetails_plumber extends AppCompatActivity {
                                 .show();
 
                     }else{
-                        Toast.makeText(historyviewdetails_plumber.this, "This complaint is not made by you so you cant close it", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(historyviewdetails_plumber.this, "You are unable to close this complaint , as it is not filled by you.", Toast.LENGTH_SHORT).show();
                     }
-                    Toast.makeText(historyviewdetails_plumber.this, "This complaint is already solved and closed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(historyviewdetails_plumber.this, "It has already been solved and closed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -311,7 +311,9 @@ public class historyviewdetails_plumber extends AppCompatActivity {
 
     private void sendMessage() {
         String sphone=staff_mob.getText().toString().trim();
-        String sMessage="Complaint Registered by has been solved Please check \n If not solved you can make the complaint again pending";
+        String sMessage="The complaint filled has been solved. \n" +
+                " check before closing the complaint \n" +
+                " If not, place it as pending";
 
         if(!sphone.equals("") && !sMessage.equals("")){
             SmsManager smsManager=SmsManager.getDefault();

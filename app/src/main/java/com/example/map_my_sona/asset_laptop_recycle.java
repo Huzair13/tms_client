5c8de5e3d4d559b4655b14584eb0d69d13311c5a
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-05-25 12:53:43 +0530

    feat : assets

---
+++ app/src/main/java/com/example/map_my_sona/asset_laptop_recycle.java
@@ -0,0 +1,29 @@
+package com.example.map_my_sona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class asset_laptop_recycle extends AppCompatActivity {
+    MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset_laptop_recycle);
+
+        toolbar= findViewById(R.id.topAppBar);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(asset_laptop_recycle.this, Assets.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/map_my_sona/asset_phone_recycle.java
@@ -0,0 +1,29 @@
+package com.example.map_my_sona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class asset_phone_recycle extends AppCompatActivity {
+    MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset_phone_recycle);
+
+        toolbar= findViewById(R.id.topAppBar);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(asset_phone_recycle.this, Assets.class));
+            }
+        });
+    }
+}

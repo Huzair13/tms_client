7166744e24334a2ddf471c0dc91030f00838de36
vigneshwaranBS <bs.vigneshwaran@gmail.com>
2022-08-05 15:40:01 +0530

    bottom navi-chnaes

--- app/src/main/java/com/example/map_my_sona/AdminDashboard.java
+++ app/src/main/java/com/example/map_my_sona/AdminDashboard.java
@@ -1,29 +1,15 @@
 package com.example.map_my_sona;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.map_my_sona.complaints.Dep_wise_history;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.logging.MemoryHandler;
-
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-import meow.bottomnavigation.MeowBottomNavigation;
 
 public class AdminDashboard extends AppCompatActivity {
 
@@ -31,10 +17,13 @@ public class AdminDashboard extends AppCompatActivity {
     private  MaterialCardView admin_dash_update;
     private MaterialCardView admin_dash_history;
 
-    private final int home= 1;
-    private final int report= 1;
-    private final int rating= 1;
-    private final int logout= 1;
+    //
+    BottomNavigationView bottomNavigation;
+
+        private final int home=1;
+        private final int report =2;
+        private final int rating=3;
+       private final int logout =4;
 
 
     @Override
@@ -48,8 +37,7 @@ public class AdminDashboard extends AppCompatActivity {
         admin_dash_history = findViewById(R.id.admin_dash_history);
 
         //bottom
-//        TextView textview = findViewById(R.id.page)
-        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomnavi);
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomnavi);
 
         admin_dash_history.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,21 +61,56 @@ public class AdminDashboard extends AppCompatActivity {
             }
         });
 
-//        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.adminicon));
 //        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.google));
 //        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.addp));
+//        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.adminicon));
 //        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.notification));
 
 
-    bottomNavigation.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //user navi
-        }
-    });
+//        bottomNavigation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//            }
+//        });
+//
+//      bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+//          @Override
+//          public void onShowItem(MeowBottomNavigation.Model item) {
+//              String name;
+//              switch (item.getId()){
+//                  case home:
+//                      break;
+//
+//                  case report:
+//                      startActivity(new Intent(AdminDashboard.this ,Qr_id_generator.class));
+//                      break;
+//                  case rating:
+//                      startActivity(new Intent(AdminDashboard.this ,Qr_id_generator.class));
+//                      break;
+//
+//                  case logout:
+//                      startActivity(new Intent(AdminDashboard.this ,Qr_id_generator.class));
+//                      break;
+//              }
+//
+//          }
+//      });
+//      bottomNavigation.show(home,true);
 
 
 
+//
+//        bottomNavigation.setOnNavigationItemReselectedListener { item ->
+//                when(item.itemId) {
+//            R.id.item1 -> {
+//                // Respond to navigation item 1 reselection
+//            }
+//            R.id.item2 -> {
+//                // Respond to navigation item 2 reselection
+//            }
+//        }
+//        }
     }
-
     }

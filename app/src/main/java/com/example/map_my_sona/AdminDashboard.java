a0fa23ebdb2c6858d1a8b2ff351fc82176c0f060
vigneshwaranBS <bs.vigneshwaran@gmail.com>
2022-08-05 13:44:32 +0530

    bottom navi

--- app/src/main/java/com/example/map_my_sona/AdminDashboard.java
+++ app/src/main/java/com/example/map_my_sona/AdminDashboard.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.map_my_sona.complaints.Dep_wise_history;
@@ -18,11 +19,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.logging.MemoryHandler;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import meow.bottomnavigation.MeowBottomNavigation;
+
 public class AdminDashboard extends AppCompatActivity {
 
     private MaterialCardView admin_dash_assign;
     private  MaterialCardView admin_dash_update;
     private MaterialCardView admin_dash_history;
+
+    private final int home= 1;
+    private final int report= 1;
+    private final int rating= 1;
+    private final int logout= 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,10 @@ public class AdminDashboard extends AppCompatActivity {
         admin_dash_assign=findViewById(R.id.admin_dash_assign);
         admin_dash_update = findViewById(R.id.admin_dash_update);
         admin_dash_history = findViewById(R.id.admin_dash_history);
+
+        //bottom
+//        TextView textview = findViewById(R.id.page)
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomnavi);
 
         admin_dash_history.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +72,21 @@ public class AdminDashboard extends AppCompatActivity {
                 startActivity(new Intent(AdminDashboard.this, Admin_Update.class));
             }
         });
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.adminicon));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.google));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.addp));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.notification));
+
+
+    bottomNavigation.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //user navi
+        }
+    });
+
+
 
     }
 

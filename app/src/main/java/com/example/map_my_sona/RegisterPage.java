26b818d1bf61f7cf7e3a7bb1a609dd06500e8e4a
vigneshwaranBS <bs.vigneshwaran@gmail.com>
2022-08-07 13:22:06 +0530

    Ui fixes

--- app/src/main/java/com/example/map_my_sona/RegisterPage.java
+++ app/src/main/java/com/example/map_my_sona/RegisterPage.java
@@ -2,13 +2,27 @@ package com.example.map_my_sona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterPage extends AppCompatActivity {
-
+    TextView alreadyreg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        alreadyreg=findViewById(R.id.alreadyreg);
+
+        alreadyreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterPage.this, loginpage.class));
+                //Toast.makeText(AdminDashboard.this, us, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

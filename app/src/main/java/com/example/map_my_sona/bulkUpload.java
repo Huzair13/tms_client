097a3e81915296b06f84f33e406910956e9d8b54
sriee19 <srisanjanaarunkumar@gmail.com>
2022-10-17 11:51:57 +0530

    register page

--- app/src/main/java/com/example/map_my_sona/bulkUpload.java
+++ app/src/main/java/com/example/map_my_sona/bulkUpload.java
@@ -3,23 +3,29 @@ package com.example.map_my_sona;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class bulkUpload extends AppCompatActivity {
 
+public class bulkUpload extends AppCompatActivity {
+    MediaController mediaControls;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulk_upload);
         setupHyperlink();
 
-
         VideoView videoView = findViewById(R.id.video_view);
+
+
+
+        //
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
         Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
@@ -28,6 +34,8 @@ public class bulkUpload extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+
 
 
     }

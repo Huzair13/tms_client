b1601db977a73d5b7f4bcd827104c982dbda3718
Huzair13 <ahamedhuzair13@gmail.com>
2022-11-03 19:55:53 +0530

    changes made

--- app/src/main/java/com/example/map_my_sona/complaints/viewDetails/historyviewdetails.java
+++ app/src/main/java/com/example/map_my_sona/complaints/viewDetails/historyviewdetails.java
@@ -44,6 +44,12 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -650,18 +656,79 @@ public class historyviewdetails extends AppCompatActivity {
 
 
     private void sendMessage() {
-        String sphone=staff_mob.getText().toString().trim();
-        String sMessage="The complaint filled has been solved. \n check before closing the complaint \n If not, place it as pending";
-
-//        if(!sphone.equals("") && !sMessage.equals("")){
+//        String sphone=staff_mob.getText().toString().trim();
+//        String sMessage="The complaint filled has been solved. \n check before closing the complaint \n If not, place it as pending";
 //
+////        if(!sphone.equals("") && !sMessage.equals("")){
+////
+////        }
+//        try{
+//            SmsManager smsManager=SmsManager.getDefault();
+//            smsManager.sendTextMessage(sphone,null,sMessage,null,null);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            Toast.makeText(this, "Failed to send message", Toast.LENGTH_SHORT).show();
 //        }
-        try{
-            SmsManager smsManager=SmsManager.getDefault();
-            smsManager.sendTextMessage(sphone,null,sMessage,null,null);
-        }catch (Exception e){
+
+        // Replace with your username
+        String user = "xxyy";
+
+        // Replace with your API KEY (We have sent API KEY on activation email, also available on panel)
+        String apikey = "xxyy";
+
+        // Replace with the destination mobile Number to which you want to send sms
+        String mobile = "9677381857";
+
+        // Replace if you have your own Sender ID, else donot change
+        String senderid = "SONACT";
+
+        // Replace with your Message content
+        String message = "Test sms API";
+
+        // For Plain Text, use "txt" ; for Unicode symbols or regional Languages like hindi/tamil/kannada use "uni"
+        String type="txt";
+
+        //Prepare Url
+        URLConnection myURLConnection=null;
+        URL myURL=null;
+        BufferedReader reader=null;
+
+        //encoding message
+        String encoded_message= URLEncoder.encode(message);
+
+        //Send SMS API
+        String mainUrl="http://smshorizon.co.in/api/sendsms.php?";
+
+        //Prepare parameter string
+        StringBuilder sbPostData= new StringBuilder(mainUrl);
+        sbPostData.append("user="+user);
+        sbPostData.append("&apikey="+apikey);
+        sbPostData.append("&message="+encoded_message);
+        sbPostData.append("&mobile="+mobile);
+        sbPostData.append("&senderid="+senderid);
+        sbPostData.append("&type="+type);
+
+        //final string
+        mainUrl = sbPostData.toString();
+        try
+        {
+            //prepare connection
+            myURL = new URL(mainUrl);
+            myURLConnection = myURL.openConnection();
+            myURLConnection.connect();
+            reader= new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+            //reading response
+            String response;
+            while ((response = reader.readLine()) != null)
+                //print response
+                System.out.println(response);
+
+            //finally close connection
+            reader.close();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
-            Toast.makeText(this, "Failed to send message", Toast.LENGTH_SHORT).show();
         }
     }
 
